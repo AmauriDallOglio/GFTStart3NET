a92228edea44350be08e74e74e584456c144c980
Amauri Dall'Oglio <amauridalloglio@outlook.com.br>
2023-10-08 12:51:27 -0300

    Adicionar arquivos de projeto.

---
+++ GFTStart3NET/Desafios/Diferenca.cs
@@ -0,0 +1,55 @@
+﻿namespace GFTStart3NET.Desafios
+{
+    public class Diferenca
+    {
+        public void Executar()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+
+            //continue a solução
+            Console.WriteLine("DIFERENCA = " + ((a * b) - (c * d)));
+            Console.ReadKey();
+
+        }
+    }
+
+
+    /*
+     * 
+     * Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+
+Entrada
+O arquivo de entrada contém 4 valores inteiros.
+
+Saída
+Imprima a mensagem DIFERENCA com todas as letras maiúsculas, conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade.
+
+ 
+Exemplos de Entrada	Exemplos de Saída
+5
+6
+7
+8
+
+DIFERENCA = -26
+
+0
+0
+7
+8
+
+DIFERENCA = -56
+
+5
+6
+-7
+8
+
+DIFERENCA = 86
+     * 
+     * 
+     */
+}
---
+++ GFTStart3NET/Program.cs
@@ -0,0 +1,12 @@
+﻿using GFTStart3NET.Desafios;
+
+Diferenca diferenca = new Diferenca();
+diferenca.Executar();
+
+Multiplos multiplos = new Multiplos();
+multiplos.Executar();
+
+CalculoSimples calculoSimples = new CalculoSimples();
+calculoSimples.Executar();
+
+
